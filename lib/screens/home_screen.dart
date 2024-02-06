ad1807295b002812774ee241df8bb92b3007c38f
yosyosyos100 <156672672+yosyosyos100@users.noreply.github.com>
2024-02-06 16:27:42 +0700

    Add files via upload

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('App Screen'),
+          centerTitle: true,
+        ),
+        body: const Center(
+          child: Text(
+            'First App Screen',
+            style: TextStyle(fontSize: 25),
+          ),
+        ));
+  }
+}
